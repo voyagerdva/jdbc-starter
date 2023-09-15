8ec55afb90bbe98be3f96bc2ef5ffc5285d6b9fe
voyagerdva <mariner-9@yandex.ru>
2023-09-15 18:27:22 +0300

    qwe1

--- src/nn/ru/jdbc/starter/JdbcRunner.java
+++ src/nn/ru/jdbc/starter/JdbcRunner.java
@@ -6,13 +6,28 @@ import org.postgresql.Driver;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
 
-        try (Connection connection = ConnectionManager.open()) {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS info (
+                    id SERIAL PRIMARY KEY ,
+                    data TEXT NOT NULL
+                    
+                )
+                                
+                """;
+
+        try (Connection connection = ConnectionManager.open();
+             Statement statement = connection.createStatement();
+             ) {
+            System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
+            boolean executeResult = statement.execute(sql);
+            System.out.println(executeResult);
         }
     }
 }
